b94adac299af83b1dd40a9a3d43787d62bb7ea7d
olaniyanp11 <olaniyanp11@gmail.com>
2023-09-13 16:46:12 +0100

    modified task 0

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,9 +1,15 @@
+#!/usr/bin/env python3
+"""
+Basic Flask app
+"""
+
 from flask import Flask,render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    """ the index page"""
     return render_template('index.html')
 
 if __name__ == "__main__":
